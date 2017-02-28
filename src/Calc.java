305c3403c7f8c7ae11a02e972848a634283233ab
richmoet <richmoet@miamioh.edu>
2017-02-28 16:28:25 -0500

    Update Calc.java

--- src/Calc.java
+++ src/Calc.java
@@ -1,1 +1,30 @@
+// Introduction to Software Testing
+// Authors: Paul Ammann & Jeff Offutt
+// Chapter 3; page ??
+// See CalcTest.java, DataDrivenCalcTest.java for JUnit tests
+
+public class Calc {
+
+	public static void main(String[] args){
+	}
+   public static int add(int a, int b) {
+      return a + b;
+   }
+  
+	   public static int subtract(int a, int b) {
+	   return Math.subtractExact(a, b);
+   }
+   
+   public static int multiply(int a, int b) {
+	   return Math.multiplyExact(a, b);
+   }
+   
+   public static int divide(int a, int b) {
+	   if (b == 0) {
+		   throw new ArithmeticException("Cannot divide by zero.");
+	   }
+	   
+	   return a / b;
+   }
+}
 
